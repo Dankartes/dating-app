2fda554c66e7d1b0dd150459993ad0c6edc2c8f9
Robert <c.robertstancu@yahoo.com>
2023-10-18 12:08:49 +0300

    completing walking skeleton

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,12 +11,11 @@ export class AppComponent implements OnInit {
   users: any;
 
   constructor(private http: HttpClient) {}
-
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: (response) => (this.users = response),
       error: (error) => console.log(error),
-      complete: () => console.log('request has completed'),
+      complete: () => console.log('Request Complted'),
     });
   }
 }
